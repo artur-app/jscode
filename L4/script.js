0f7cbb61f2389d8c4acf7543316fa392bfc888e8
Artur Akhmetshin <artur.akhmetshin@outlook.com>
2022-04-17 19:28:18 +0300

    fix: calculator connect to js

---
+++ L4/script.js
@@ -0,0 +1,16 @@
+function makeSum()
+{
+    let NuOne = document.getElementById("numberOne").value;
+    let NuTwo = document.getElementById("numberTwo").value;
+    document.getElementById("Result").textContent = NuOne + NuTwo;
+    document.write (NuOne + NuTwo);
+}
+
+
+function makeMult()
+{
+    let NuOne = document.getElementById("numberOne").value;
+    let NuTwo = document.getElementById ("numberTwo").value;
+    document.getElementById("Result").textContent = NuOne * NuTwo;
+    document.write (NuOne + NuTwo);
+}
